b6183594f4c202a5b415ee3924aaab587bc18548
Nytierra123 <nytierrahobbs@gmail.com>
2025-06-09 21:49:08 -0400

    Clean project commit with .gitignore applied

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        try
+        {
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            driver.FindElement(By.ClassName("radius")).Click();
+
+            System.Threading.Thread.Sleep(2000); // Wait for the page to load
+
+            if (driver.Url.Contains("/secure"))
+                Console.WriteLine("✅ Login Test Passed.");
+            else
+                Console.WriteLine("❌ Login Test Failed.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("❗ Error: " + ex.Message);
+        }
+        finally
+        {
+            driver.Quit();
+        }
+    }
+}
+
